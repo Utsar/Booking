be6a9bd88843e41cfc7c5a31ca6711df4014e918
Kristian Utsar <utsaru@gmail.com>
2022-04-27 11:51:36 +0100

    quick setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Hotel from "./pages/hotel/Hotel";
+import List from "./pages/list/list";
+
 function App() {
   return (
-    <div>
-      Hello world!
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<List />} />
+        <Route path="/hotels/:id" element={<Hotel />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
